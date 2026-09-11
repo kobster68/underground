edf292faee6bcaec2326c2ed73f2abb5d1e0eb8b
Kobe LaPrade <41244097+kobster68@users.noreply.github.com>
2026-09-11 01:10:19 -0400

    feat: basic vehicle controls

--- scripts/vehicle_controller.gd
+++ scripts/vehicle_controller.gd
@@ -1,4 +1,15 @@
+class_name VehicleController
 extends Node2D
 
+var throttle_input : float = 0.0
+var brake_input : float = 0.0
+var steering_input : float = 0.0
+
 func _process(delta: float) -> void:
-	pass
+	throttle_input = Input.get_action_raw_strength("throttle")
+	brake_input = Input.get_action_raw_strength("brake")
+	steering_input = (Input.get_action_raw_strength("steering_right") -
+		Input.get_action_raw_strength("steering_left"))
+		
+	position += transform.x * throttle_input * delta * 50.0
+	rotation += steering_input * delta
